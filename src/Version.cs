b2617663ee4f9f3f735a6f820cba4579643dd664
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-19 15:35:33 -0300

    Create separate v and vpre endpoints, like shields.io
    This allows an easier mapping and then our label filter is just one more arg.
    
    Examples:
    
    https://img.shields.io/endpoint?url=https://shields.kzu.io/v/Stunts?f=api.nuget.org/v3/index.json
    
    https://img.shields.io/endpoint?url=https://shields.kzu.io/vpre/Stunts?f=api.nuget.org/v3/index.json
    
    https://img.shields.io/endpoint?url=https://shields.kzu.io/vpre/Stunts/alpha?f=api.nuget.org/v3/index.json

--- src/Version.cs
+++ src/Version.cs
@@ -19,9 +19,56 @@ namespace Shields
     {
         const string defaultFeed = "https://pkg.kzu.io/index.json";
 
-        [FunctionName("version")]
+        [FunctionName("v")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "version/{id}/{label?}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v/{id}")] HttpRequest req,
+            ILogger log, string id)
+        {
+            var feedUrl = req.Query.TryGetValue("feed", out var feed) ?
+                feed.FirstOrDefault() ?? defaultFeed :
+                req.Query.TryGetValue("f", out var f) ?
+                f.FirstOrDefault() ?? defaultFeed :
+                defaultFeed;
+
+            if (!feedUrl.StartsWith("http://") && !feedUrl.StartsWith("https://"))
+                feedUrl = "https://" + feedUrl;
+
+            var providers = Repository.Provider.GetCoreV3();
+            var source = new PackageSource(feedUrl);
+            var repo = new SourceRepository(source, providers);
+            var resource = await repo.GetResourceAsync<PackageMetadataResource>();
+            var metadata = await resource.GetMetadataAsync(id, false, false, NuGet.Common.NullLogger.Instance, CancellationToken.None);
+            var package = metadata.Select(m => m.Identity).OrderByDescending(m => m.Version).FirstOrDefault();
+
+            if (package == null)
+                return new ContentResult
+                {
+                    Content = JsonConvert.SerializeObject(new
+                    {
+                        schemaVersion = 1,
+                        label = id + ".404",
+                        message = "NotFound",
+                    }),
+                    ContentType = "application/json",
+                    StatusCode = 200
+                };
+
+            return new ContentResult
+            {
+                Content = JsonConvert.SerializeObject(new
+                {
+                    schemaVersion = 1,
+                    label = id,
+                    message = package.Version.ToNormalizedString(),
+                }),
+                ContentType = "application/json",
+                StatusCode = 200
+            };
+        }
+
+        [FunctionName("vpre")]
+        public static async Task<IActionResult> RunPre(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "vpre/{id}/{label?}")] HttpRequest req,
             ILogger log, string id, string? label)
         {
             var feedUrl = req.Query.TryGetValue("feed", out var feed) ?
@@ -37,14 +84,9 @@ namespace Shields
             var source = new PackageSource(feedUrl);
             var repo = new SourceRepository(source, providers);
             var resource = await repo.GetResourceAsync<PackageMetadataResource>();
-            var metadata = await resource.GetMetadataAsync(id, !string.IsNullOrEmpty(label), false, NuGet.Common.NullLogger.Instance, CancellationToken.None);
+            var metadata = await resource.GetMetadataAsync(id, true, false, NuGet.Common.NullLogger.Instance, CancellationToken.None);
 
-            // 3.1.0 is already stable and we verified we work with it
-            // Older versions are guaranteed to not change either, so we 
-            // can rely on it working too, since this test passed at some 
-            // point too.
             IEnumerable<PackageIdentity> query = metadata.Select(m => m.Identity).OrderByDescending(m => m.Version);
-
             if (!string.IsNullOrEmpty(label))
                 query = query.Where(m => m.Version.IsPrerelease && m.Version.ReleaseLabels.Any(l => l.StartsWith(label)));
 
