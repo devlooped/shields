650a090f27ab9fd1809c828deccd35cde3f41964
Daniel Cazzulino <daniel@cazzulino.com>
2020-10-19 15:21:31 -0300

    Allow passing feed url too
    Usage:
    
    https://img.shields.io/endpoint?url=https://shields.kzu.io/version/MyPackage/MyLabel
    
    Examples:
    
    https://img.shields.io/endpoint?url=https://shields.kzu.io/version/Microsoft.CodeAnalysis/beta?f=api.nuget.org/v3/index.json
    
    https://img.shields.io/endpoint?url=https://shields.kzu.io/version/ThisAssembly/beta?f=api.nuget.org/v3/index.json

--- src/Version.cs
+++ src/Version.cs
@@ -11,33 +11,45 @@ using NuGet.Configuration;
 using NuGet.Protocol;
 using System.Threading;
 using System.Linq;
+using NuGet.Packaging.Core;
+using System.Collections.Generic;
 
 namespace Shields
 {
     public static class Version
     {
-        const string feedUrl = "https://pkg.kzu.io/index.json";
+        const string defaultFeed = "https://pkg.kzu.io/index.json";
 
         [FunctionName("version")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "version/{id}/{label}")] HttpRequest req,
-            ILogger log, string id, string label)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "version/{id}/{label?}")] HttpRequest req,
+            ILogger log, string id, string? label)
         {
+            var feedUrl = req.Query.TryGetValue("feed", out var feed) ?
+                feed.FirstOrDefault() ?? defaultFeed :
+                req.Query.TryGetValue("f", out var f) ?
+                f.FirstOrDefault() ?? defaultFeed :
+                defaultFeed;
+
+            if (!feedUrl.StartsWith("http://") || !feedUrl.StartsWith("https://"))
+                feedUrl = "https://" + feedUrl;
+
             var providers = Repository.Provider.GetCoreV3();
             var source = new PackageSource(feedUrl);
             var repo = new SourceRepository(source, providers);
             var resource = await repo.GetResourceAsync<PackageMetadataResource>();
-            var metadata = await resource.GetMetadataAsync(id, true, false, NuGet.Common.NullLogger.Instance, CancellationToken.None);
+            var metadata = await resource.GetMetadataAsync(id, !string.IsNullOrEmpty(label), false, NuGet.Common.NullLogger.Instance, CancellationToken.None);
 
             // 3.1.0 is already stable and we verified we work with it
             // Older versions are guaranteed to not change either, so we 
             // can rely on it working too, since this test passed at some 
             // point too.
-            var package = metadata
-                .Select(m => m.Identity)
-                .Where(m => m.Version.IsPrerelease && m.Version.ReleaseLabels.Contains(label))
-                .OrderByDescending(m => m.Version)
-                .FirstOrDefault();
+            IEnumerable<PackageIdentity> query = metadata.Select(m => m.Identity).OrderByDescending(m => m.Version);
+
+            if (!string.IsNullOrEmpty(label))
+                query = query.Where(m => m.Version.IsPrerelease && m.Version.ReleaseLabels.Any(l => l.StartsWith(label)));
+
+            var package = query.FirstOrDefault();
 
             if (package == null)
                 return new ContentResult
